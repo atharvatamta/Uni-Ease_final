c5329e36788c5001b7c4a5786e54bcb1e71f6963
Yogita Das <yogitadas24@gmail.com>
2024-11-12 18:03:44 +0530

    Some changes in NavBar and footer

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the Academic Resources card element
-    const academicCard = document.querySelector('.academic-card');
-  
-    // Add click event listener
-    academicCard.addEventListener('click', function() {
-      // Open the new page in a new tab
-      window.open('academic-resources.html', '_blank'); // Replace with your actual page URL
-    });
-  });
-  
+  // Get the Academic Resources card element
+  const academicCard = document.querySelector('.academic-card');
+
+  // Add click event listener
+  academicCard.addEventListener('click', function() {
+    // Open the new page in a new tab
+    window.location.href = 'academic-resources.html';
+});
+});
